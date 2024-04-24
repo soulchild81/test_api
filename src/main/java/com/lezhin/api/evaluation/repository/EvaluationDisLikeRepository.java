00354988029aef4b424e74ce0a87342e2c2f2b21
JONGOK <soulchild81@naver.com>
2024-04-24 14:55:17 +0900

    nativeQuery 수정 alias 추가

--- src/main/java/com/lezhin/api/evaluation/repository/EvaluationDisLikeRepository.java
+++ src/main/java/com/lezhin/api/evaluation/repository/EvaluationDisLikeRepository.java
@@ -12,10 +12,10 @@ import java.util.List;
 
 public interface EvaluationDisLikeRepository  extends JpaRepository<ContentDisLike, Long> {
     @Modifying
-    @Query(nativeQuery = true, value = "DELETE FROM CONTENT_DIS_LIKE where user_seq = :userSeq")
+    @Query(nativeQuery = true, value = "DELETE FROM soulchild_content.CONTENT_DIS_LIKE where user_seq = :userSeq")
     public void removeDisLikeUser(@Param("userSeq") long userSeq);
-    @Query(nativeQuery = true, value = "SELECT count(content_seq) as disLikeCount FROM CONTENT_DIS_LIKE where content_seq = :contentSeq")
+    @Query(nativeQuery = true, value = "SELECT count(content_seq) as disLikeCount FROM soulchild_content.CONTENT_DIS_LIKE where content_seq = :contentSeq")
     public EvaluationCount findContentDisLikeCount(@Param("contentSeq") long contentSeq);
-    @Query(nativeQuery = true, value = " SELECT ia.content_seq as contentSeq, ia.like_count as likeCount FROM (SELECT cl.content_seq as content_seq, count(*) as like_count FROM content_dis_like cl GROUP BY cl.content_seq) as ia ORDER BY ia.like_count desc , ia.content_seq asc")
+    @Query(nativeQuery = true, value = " SELECT ia.content_seq as contentSeq, ia.like_count as likeCount FROM (SELECT cl.content_seq as content_seq, count(*) as like_count FROM soulchild_content.content_dis_like cl GROUP BY cl.content_seq) as ia ORDER BY ia.like_count desc , ia.content_seq asc")
     public List<EvaluationCount> findRank(Pageable pageable);
 }
--- src/main/java/com/lezhin/api/evaluation/repository/EvaluationLikeRepository.java
+++ src/main/java/com/lezhin/api/evaluation/repository/EvaluationLikeRepository.java
@@ -16,11 +16,11 @@ import java.util.Map;
 @Repository
 public interface EvaluationLikeRepository extends JpaRepository<ContentLike, Long> {
     @Modifying
-    @Query(nativeQuery = true, value = "DELETE FROM CONTENT_LIKE where user_seq = :userSeq")
+    @Query(nativeQuery = true, value = "DELETE FROM soulchild_content.CONTENT_LIKE where user_seq = :userSeq")
     public void removeLikeUser(@Param("userSeq") long userSeq);
-    @Query(nativeQuery = true, value = "SELECT count(content_seq) as likeCount FROM CONTENT_LIKE where content_seq = :contentSeq")
+    @Query(nativeQuery = true, value = "SELECT count(content_seq) as likeCount FROM soulchild_content.CONTENT_LIKE where content_seq = :contentSeq")
     public EvaluationCount findContentLikeCount(@Param("contentSeq") long contentSeq);
 
-    @Query(nativeQuery = true, value = " SELECT ia.content_seq as contentSeq, ia.like_count as likeCount FROM (SELECT cl.content_seq as content_seq, count(*) as like_count FROM content_like cl GROUP BY cl.content_seq) as ia ORDER BY ia.like_count desc , ia.content_seq asc")
+    @Query(nativeQuery = true, value = " SELECT ia.content_seq as contentSeq, ia.like_count as likeCount FROM (SELECT cl.content_seq as content_seq, count(*) as like_count FROM soulchild_content.content_like cl GROUP BY cl.content_seq) as ia ORDER BY ia.like_count desc , ia.content_seq asc")
     public List<EvaluationCount> findRank(Pageable pageable);
 }
